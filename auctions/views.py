cc8fe272ae42178bb3ae542e0a570b63ac940e09
berordas <202214793@alu.comillas.edu>
2025-05-15 16:45:20 +0200

    Corrección de errores.

--- auctions/views.py
+++ auctions/views.py
@@ -24,7 +24,7 @@ from rest_framework.response import Response
 from .permissions import IsOwnerOrAdmin, IsAuthenticatedOrReadOnly, IsAdminOrReadOnly
 
 class CategoryListCreate(generics.ListCreateAPIView):
-    permission_classes = [IsAdminOrReadOnly]
+    # permission_classes = [IsAdminOrReadOnly]
     queryset = Category.objects.all()
     serializer_class = CategoryListCreateSerializer
 
